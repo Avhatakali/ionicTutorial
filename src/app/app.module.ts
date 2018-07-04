eb37f5648b022d6e9c4ed39f8364f72ed6241ca7
Avhatakali <avhatakali51@gmail.com>
2018-07-04 16:20:50 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InstallationComponent } from './installation/installation.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { HomeComponent } from './home/home.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InstallationComponent,
+    IntroductionComponent,
+    HomeComponent,
+    TutorialComponent
   ],
+  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'', component: HomeComponent},
+      {path:'introduction', component: IntroductionComponent},
+      {path:'installation', component: InstallationComponent},
+      {path:'tutorial', component: TutorialComponent},
+
+      // {path:'contact', component: ContactComponent},
+      // {path:'news', component: NewsComponent},
+  
+      ])  
   ],
   providers: [],
   bootstrap: [AppComponent]
